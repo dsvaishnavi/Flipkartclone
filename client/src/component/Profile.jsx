4868cd2dabd889d5c58cf718bcd4221560616ef1
dsvaishnavi <vaishnavidhawade1@gmail.com>
2025-02-09 19:43:44 +0530

    update

--- client/src/component/Profile.jsx
+++ client/src/component/Profile.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { FaPowerOff } from "react-icons/fa6";
+import { CgProfile } from "react-icons/cg";
 
 export const Profile = ({ acc, onLogout }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,17 +17,19 @@ export const Profile = ({ acc, onLogout }) => {
 
       {/* Dropdown Menu */}
       {isOpen && (
-        <div className="absolute right-0 mt-2 w-48 bg-white rounded-lg shadow-lg overflow-hidden border">
+        <div className="absolute right-0 mt-2 w-40 bg-white rounded-lg shadow-lg overflow-hidden border">
           <button
             onClick={onLogout}
-            className="block w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-200"
+            className="block w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-300"
           >
+            <CgProfile className="inline-block mr-2"/>
             Profile
           </button>
           <button
             onClick={onLogout}
-            className="block w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-200"
+            className="block w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-300"
           >
+            <FaPowerOff className="inline-block mr-2" />
             Logout
           </button>
         </div>
